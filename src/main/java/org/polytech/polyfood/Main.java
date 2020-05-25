be1dfa5c33f40d8327f44f7e0abbdac16a64d722
zeroual <abid1991@hotmail.fr>
2020-05-25 11:48:01 +0200

    use jdbc to fetch elements from database

--- src/main/java/org/polytech/polyfood/Main.java
+++ src/main/java/org/polytech/polyfood/Main.java
@@ -3,30 +3,34 @@ package org.polytech.polyfood;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-
-        String url="jdbc:mysql://localhost:3306/polyfood";
-
-        String username="root";
-        String password="rootpw";
-
+        String url = "jdbc:mysql://localhost:3306/polyfood";
+        String username = "root";
+        String password = "rootpw";
+        System.out.println("Hello World");
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-            String sql = "INSERT INTO USERS VALUES('abdel2', 'pwd' )";
 
-            connection.createStatement().execute(sql);
+            String sql = "SELECT * FROM USERS";
+
+            // execute --> update, delete, insert
+            // executeQuery --> select
+
+            ResultSet resultSet = connection.createStatement().executeQuery(sql);
+            // resultSet is the result of select query
+            while (resultSet.next()) {
+                System.out.println("username:" + resultSet.getString("username") + ",password:" + resultSet.getString("password"));
+            }
 
             connection.close();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
-
 }
