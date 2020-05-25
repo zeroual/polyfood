d11efd447b33acbfdd9693b682891498fdd5278a
zeroual <abid1991@hotmail.fr>
2020-05-25 10:50:57 +0200

    use jdbc to insert elements in database

--- src/main/java/org/polytech/polyfood/Main.java
+++ src/main/java/org/polytech/polyfood/Main.java
@@ -1,10 +1,32 @@
 package org.polytech.polyfood;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        String url="jdbc:mysql://localhost:3306/polyfood";
+
+        String username="root";
+        String password="rootpw";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            String sql = "INSERT INTO USERS VALUES('abdel2', 'pwd' )";
+
+            connection.createStatement().execute(sql);
+
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
